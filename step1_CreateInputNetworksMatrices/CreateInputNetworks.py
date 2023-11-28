0452c75c3f60c7f9e86c4ecefe090d20834ce25b
KatarinaMihajlovic <katarina.mihajlovic@bsc.es>
2023-11-28 12:55:15 +0100

    update step1

--- step1_CreateInputNetworksMatrices/CreateInputNetworks.py
+++ step1_CreateInputNetworksMatrices/CreateInputNetworks.py
@@ -57,11 +57,15 @@ basefolderIN = 'input/'
 basefolderOUT = 'output/'   
 
 
-EXPs = pd.read_csv(f'{basefolderIN}/ExpressionMatrix/Normalized_data.zip', index_col=0)
+EXPs_ctrl = pd.read_csv('input/ExpressionMatrix/Normalized_data_Control.zip', index_col=0)
+EXPs_pink1 = pd.read_csv('input/ExpressionMatrix/Normalized_data_PINK1.zip', index_col=0)
+EXPs = pd.concat([EXPs_ctrl,EXPs_pink1],axis=1)
 genes_SC = EXPs.index.tolist()
 
 Metadata_file = open(f'{basefolderIN}/ExpressionMatrix/Metadata.csv','r')
 cell_conds_2_singlecells, cell_conds = cell_conds2singlecells(Metadata_file)
+del cell_conds_2_singlecells['Control_D10']
+cell_conds.remove('Control_D10')
 
 gen_nets_dir = 'input/GeneralNetworks'
 GeneralNets = os.listdir(gen_nets_dir)
@@ -72,54 +76,52 @@ nx.write_edgelist(PPI, 'output/PPI_General_Biogrid_GeneSym.edgelist', data=False
 
 
 for cell_cond in cell_conds:
-    if cell_cond != 'Control_D10':
-        print(cell_cond)
+    print(cell_cond)
 
-        if not os.path.exists(f'output/MolecularEXPSpecificNetworks/{cell_cond}'):
-            os.makedirs(f'output/MolecularEXPSpecificNetworks/{cell_cond}') 
+    if not os.path.exists(f'output/MolecularEXPSpecificNetworks/{cell_cond}'):
+        os.makedirs(f'output/MolecularEXPSpecificNetworks/{cell_cond}') 
 
-        savedir_nets = f'{basefolderOUT}/MolecularEXPSpecificNetworks/{cell_cond}'
-        
-        EXP_cellcond = pd.DataFrame(index=EXPs.index)
-        SCs = []
-        data = []
-        for SC in EXPs:
-            if SC in cell_conds_2_singlecells[cell_cond]:
-                SCs.append(SC)
-                data.append(list(EXPs[SC]))
-        data = np.array(data)
-        data = data.T        
-        EXP_cellcond = pd.DataFrame(data, index=EXPs.index, columns = SCs)
-        EXP_values = EXP_cellcond.values.tolist()
+    savedir_nets = f'{basefolderOUT}/MolecularEXPSpecificNetworks/{cell_cond}'
+    
+    EXP_cellcond = pd.DataFrame(index=EXPs.index)
+    SCs = []
+    data = []
+    for SC in EXPs:
+        if SC in cell_conds_2_singlecells[cell_cond]:
+            SCs.append(SC)
+            data.append(list(EXPs[SC]))
+    data = np.array(data)
+    data = data.T        
+    EXP_cellcond = pd.DataFrame(data, index=EXPs.index, columns = SCs)
+    EXP_values = EXP_cellcond.values.tolist()
 
-        zero_genes = []
-        for i in range(len(EXP_values)):
-            row = EXP_values[i]
-            sum_row = sum(row)
-            if sum_row == 0:
-                zero_genes.append(genes_SC[i])
-        expressed_genes = [x for x in genes_SC if x not in zero_genes]   
-        EXP_cellcond_unique = EXP_cellcond.loc[expressed_genes, :]
+    zero_genes = []
+    for i in range(len(EXP_values)):
+        row = EXP_values[i]
+        sum_row = sum(row)
+        if sum_row == 0:
+            zero_genes.append(genes_SC[i])
+    expressed_genes = [x for x in genes_SC if x not in zero_genes]   
+    EXP_cellcond_unique = EXP_cellcond.loc[expressed_genes, :]
 
-        PPI = nx.read_edgelist(f'{gen_nets_dir}/PPI_General_Biogrid.edgelist')
-        PPI = nx.relabel_nodes(PPI, Entrez2Sym)
-        genes_PPI = list(PPI.nodes())
+    PPI = nx.read_edgelist(f'{gen_nets_dir}/PPI_General_Biogrid.edgelist')
+    PPI = nx.relabel_nodes(PPI, Entrez2Sym)
+    genes_PPI = list(PPI.nodes())
 
-        MolecularEXPSpecificNetworks_info = open(f'{savedir_nets}/MolecularEXPSpecificNetworks_Statistics_{cell_cond}.txt','w')
-        
-        for i in range(len(GeneralNets)):            
-            SpecificNet = CreateNetwork(GeneralNets[i], expressed_genes, genes_PPI, gen_nets_dir)         
-            net = GeneralNets[i].split('_')[0]
-            net_name =  f'{net}_{cell_cond}.edgelist'
-            nx.write_edgelist(SpecificNet, f'{savedir_nets}/{net_name}', data=False)
-            SpecificNet = nx.read_edgelist(f'{savedir_nets}/{net_name}')
-            print(f"genes: {SpecificNet.number_of_nodes()} \n interactions: {SpecificNet.number_of_edges()}\n density: {nx.density(SpecificNet)}\n")
+    MolecularEXPSpecificNetworks_info = open(f'{savedir_nets}/MolecularEXPSpecificNetworks_Statistics_{cell_cond}.txt','w')
+    
+    for i in range(len(GeneralNets)):            
+        SpecificNet = CreateNetwork(GeneralNets[i], expressed_genes, genes_PPI, gen_nets_dir)         
+        net = GeneralNets[i].split('_')[0]
+        net_name =  f'{net}_{cell_cond}.edgelist'
+        nx.write_edgelist(SpecificNet, f'{savedir_nets}/{net_name}', data=False)
+        SpecificNet = nx.read_edgelist(f'{savedir_nets}/{net_name}')
+        print(f"genes: {SpecificNet.number_of_nodes()} \n interactions: {SpecificNet.number_of_edges()}\n density: {nx.density(SpecificNet)}\n")
 
-            MolecularEXPSpecificNetworks_info.write(f'{net} \n genes: {SpecificNet.number_of_nodes()} \n interactions: {SpecificNet.number_of_edges()}\n density: {nx.density(SpecificNet)}\n\n')
-        MolecularEXPSpecificNetworks_info.close()   
-        
-        PPI = nx.read_edgelist(f'{savedir_nets}/PPI_{cell_cond}.edgelist')
-        # PPI = nx.relabel_nodes(PPI, Entrez2Sym)
-        genes_PPI = list(PPI.nodes())
-        EXP_cellcond_unique.to_csv(f'{basefolderOUT}/Expression_Matrix/E_{cell_cond}.csv' , header = True, index = True)
-        
+        MolecularEXPSpecificNetworks_info.write(f'{net} \n genes: {SpecificNet.number_of_nodes()} \n interactions: {SpecificNet.number_of_edges()}\n density: {nx.density(SpecificNet)}\n\n')
+    MolecularEXPSpecificNetworks_info.close()   
+    
+    PPI = nx.read_edgelist(f'{savedir_nets}/PPI_{cell_cond}.edgelist')
+    genes_PPI = list(PPI.nodes())
+    EXP_cellcond_unique.to_csv(f'{basefolderOUT}/Expression_Matrix/E_{cell_cond}.csv' , header = True, index = True)
+    
